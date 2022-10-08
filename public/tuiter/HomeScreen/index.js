04c0d2bc5090df311483486fe7cbb17eb29f46d6
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-08 01:59:50 -0400

    a5 home screen base setup

---
+++ public/tuiter/HomeScreen/index.js
@@ -0,0 +1,25 @@
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
+
+const ExploreComponent = () => {
+    $('#wd-explore').append(`
+       <div class="row mt-2">
+       
+       <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+        ${NavigationSidebar()}
+       </div>
+       
+       <div class="col-10 col-lg-7 col-xl-6 bg-primary">
+  
+            main content     
+       </div>
+       
+       <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+        ${PostSummaryList()}
+       </div>
+      </div>
+   `);
+}
+
+$(ExploreComponent);
