3fe3dd8e358995ba3dde9a3e349a6d1b9ae8a78f
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-07 23:21:47 -0400

    a5 labs

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -43,7 +43,7 @@ let true4 = numberVariable === 123;
 let true5 = floatingPointNumber !== 321.432;
 let false3 = numberVariable < 100;
 let sortaTrue = '1' == 1
-let notTrue   = '1' === 1
+let notTrue = '1' === 1
 
 console.log(true1)
 console.log(false1)
@@ -58,11 +58,11 @@ console.log(notTrue)
 
 
 console.log('If else');
-if(true1) {
+if (true1) {
     console.log(true);
 }
 
-if(!false1) {
+if (!false1) {
     console.log('!false1');
 } else {
     console.log('false1');
@@ -76,9 +76,11 @@ console.log(greeting)
 
 
 console.log('Legacy ES5 function')
-function add (a, b) {
+
+function add(a, b) {
     return a + b;
 }
+
 const twoPlusFour = add(2, 4);
 console.log(twoPlusFour);
 
@@ -141,7 +143,7 @@ console.log(stringArray1);
 
 
 console.log("For loops")
-for (let i=0; i<stringArray1.length; i++) {
+for (let i = 0; i < stringArray1.length; i++) {
     const string1 = stringArray1[i];
     console.log(string1);
 }
@@ -198,3 +200,102 @@ console.log(greeting1);
 loggedIn = false;
 const greeting2 = `Logged in: ${loggedIn ? "Yes" : "No"}`;
 console.log(greeting2)
+
+const init = () => {
+    console.log('Hello world from jQuery');
+
+    const bindById = $('#bind-by-id');
+    const bindByClass = $('.bind-by-class');
+    console.log('Binding to DOM');
+    console.log(bindById);
+    console.log(bindByClass);
+
+    const changeStyle = $('#change-style');
+    const changeStyle2 = $('.change-style');
+    changeStyle.css('color', 'red');
+    changeStyle2.css('color', 'blue');
+
+    const getIdAttr = $("#get-id-attr");
+    const id = getIdAttr.attr('id');
+    console.log(id);
+
+    const setClassAttr = $("#set-class-attr");
+    setClassAttr.attr('class', 'class-0');
+
+    const addClass1Example = $("#add-class-1");
+    addClass1Example.addClass('class-1');
+
+    const removeClass1Example = $("#remove-class-1");
+    removeClass1Example.removeClass('class-2');
+
+    const hideMe = $("#hide-me");
+    hideMe.hide();
+
+    const showMe = $("#show-me");
+    showMe.show();
+
+    const newLineItem = $("<li>Line item 1</li>");
+    const anotherLineItem = $("<li>Line item 2</li>");
+
+    const ul = $("#append-new-elements");
+    ul.append(newLineItem);
+    ul.append(anotherLineItem);
+
+    const removeLi = $("#remove-this");
+    const emptyUl = $("#empty-this");
+    removeLi.remove();
+    emptyUl.empty();
+
+    const changeThisText =
+        $("#change-this-text");
+    const changeThisHtml =
+        $("#change-this-html");
+    changeThisText.html('New text');
+    changeThisHtml.html(`
+   <li>Line item A</li>
+   <li>Line item B</li>
+   <li>Line item C</li> 
+       `);
+
+    const child2 = $("#child-2");
+    const parent1 =
+        child2.parents("#parent");
+    parent1
+        .css('background-color', 'red')
+        .css('color', 'white');
+
+    const parent = $("#parent");
+    const child = parent.find("#child-2");
+    child.css('background-color', 'blue');
+
+    const handleClick =
+        () => console.log('Handle click');
+    const clickable = $('.clickable');
+    clickable.click(handleClick);
+
+    const handleEventTarget = (event) => {
+        const target = event.target;
+        console.log(target);
+        $(target)
+            .css('background-color', 'blue')
+            .css('color', 'white');
+    }
+    const eventTarget = $("#event-target");
+    eventTarget.click(handleEventTarget);
+
+    let hideBtn, showBtn, hideShowHeader;
+    hideBtn = $('#hide');
+    showBtn = $('#show');
+    hideShowHeader = $('#hide-show');
+    const hideHandler = () => {
+        hideShowHeader.hide();
+    }
+    const showHandler = () => {
+        hideShowHeader.show();
+    }
+    hideBtn.click(hideHandler);
+    showBtn.click(showHandler);
+
+}
+
+$(init);
