308b4141d83d1ec1ce1337762f67958bbaec5ca6
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-08 02:21:33 -0400

    a5 home screen setup

---
+++ public/tuiter/PostList/PostItem.js
@@ -0,0 +1,7 @@
+const PostItem = (post) => {
+    return (`
+    
+    `);
+}
+
+export default PostItem;
---
+++ public/tuiter/PostList/posts.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        userName: 'Elon Musk',
+        handle: 'elonmusk',
+        time: '23h',
+        title: 'Amazing show about @Inspiration4x mission!',
+        profileImage: '../../images/elon_musk.jpg',
+        image: '../../images/space_suit.jpg'
+    },
+    {
+        userName: 'New York Post',
+        handle: 'nypost',
+        time: '23h',
+        title: 'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk trib.al/nx2Gfaq',
+        profileImage: '../../images/ny_post.jpg',
+        image: '../../images/grimes.jpg'
+    },
+];
