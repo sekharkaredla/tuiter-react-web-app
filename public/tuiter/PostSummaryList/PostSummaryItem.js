269c7227df396025ff26f27d54f0fd23089c8a7f
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-08 01:49:33 -0400

    a5 tuiter done

---
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -0,0 +1,24 @@
+const PostSummaryItem = (post) => {
+    return (`
+    <li class="list-group-item">
+        <div class="row">
+            <div class="col-9 col-xxl-10">
+                <div class="text-secondary">
+                    ${post.topic}
+                </div>
+                <div class="fw-bolder">
+                    ${post.userName} <i class="fa-solid fa-circle-check"></i><span class="text-secondary"> - ${post.time}</span>
+                </div>
+                <div class="fw-bolder">
+                    ${post.title}
+                </div>
+            </div>
+            <div class="col-3 col-xxl-2 align-self-center">
+                <img src="${post.image}" class="float-end rounded-2" height="80px"/>
+            </div>
+        </div>
+    </li>
+    `);
+}
+
+export default PostSummaryItem;
--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,15 +1,52 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
 
-const exploreComponent = () => {
+const ExploreComponent = () => {
     $('#wd-explore').append(`
-       <h2>Explore</h2>
        <div class="row mt-2">
+       
        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
         ${NavigationSidebar()}
        </div>
-       <div class="col-10 col-lg-7 col-xl-6 bg-primary">
-        <h3>main content</h3>
+       
+       <div class="col-10 col-lg-7 col-xl-6">
+       <div class="row">
+            <div class="col-11">
+                <div class="position-relative">
+                    <input class="ps-5 form-control rounded-pill" placeholder="Search Tuiter"/>
+                    <i class="fa-sharp fa-solid fa-magnifying-glass position-absolute wd-magnifying-glass"></i>
+                </div>
+            </div>
+            <div class="col-1">
+                <i class="fa-solid fa-gear text-primary fs-4"></i>
+            </div>
+        </div>
+
+        <ul class="nav nav-tabs mt-2 mb-2">
+            <li class="nav-item">
+                <a class="nav-link active" href="for-you.html">For You</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="trending.html">Trending</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="news.html">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="sports.html">Sports</a>
+            </li>
+            <li class="nav-item d-none d-md-block">
+                <a class="nav-link" href="entertainment.html">Entertainment</a>
+            </li>
+        </ul>
+
+        <div class="position-relative">
+            <img src="../../images/starship.png" width="100%"/>
+            <h2 class="position-absolute bottom-0 start-0 ps-2 text-white">SpaceX's Starship</h2>
+        </div>
+        
+        ${PostSummaryList()}
        </div>
        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
         ${WhoToFollowList()}
@@ -18,4 +55,4 @@ const exploreComponent = () => {
    `);
 }
 
-$(exploreComponent);
+$(ExploreComponent);
