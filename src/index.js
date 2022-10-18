c0e72a542c9152d8adc606d87284f38cd73a9c85
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-17 22:46:10 -0400

    a6 bootswatch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootswatch/dist/cyborg/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
