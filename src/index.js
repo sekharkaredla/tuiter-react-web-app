eb5c31e930acc877a187c9b772a34e75c56d5946
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-17 22:50:37 -0400

    a6 bootswatch

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootswatch/dist/cyborg/bootstrap.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
