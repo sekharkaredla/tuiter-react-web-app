7a8e95a42a1eb8118d2ba2eca5b09a937dfcb5d5
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-25 01:04:14 -0400

    a7 tuiter home page styling

--- src/tuiter/home-screen-post-content/index.js
+++ src/tuiter/home-screen-post-content/index.js
@@ -10,9 +10,7 @@ const HomeScreenPostContentComponent = ({post}) =>
                     <i className="fa-solid fa-ellipsis float-end pt-2 pb-1"></i>
                 </a>
             </div>
-            <div className="text-white">{post.title} &nbsp;&nbsp;
-                <a href={`${post.titleLink}`} className="text-primary text-decoration-none">{post.titleLinkContent}</a>
-            </div>
+            <div className="text-white">{post.title}</div>
         </div>
     </>
 
