a19c6be95d5d71b0a7ca43ef3adbb1dd965e8ce4
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-24 23:24:25 -0400

    a7 labs

--- src/nav.js
+++ src/nav.js
@@ -2,12 +2,13 @@ import {Link} from "react-router-dom";
 
 function Nav() {
     return (
-        <div className="fs-4">
-            <Link to="/"> Labs </Link> |
-            <Link to="/hello"> Hello </Link> |
-            <Link to="/tuiter/explore"> Tuiter </Link>
+        <div>
+            <Link to="/">Labs</Link> |
+            <Link to="/">Assignment 6</Link> |
+            <Link to="/a7">Assignment 7</Link> |
+            <Link to="/tuiter">Tuiter</Link>
         </div>
-    )
+    );
 }
 
 export default Nav;
