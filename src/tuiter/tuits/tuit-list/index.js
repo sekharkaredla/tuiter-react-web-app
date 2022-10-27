3b56f152043d59d84f2946b544223307d07ac161
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-27 01:02:02 -0400

    a7 adding basic reducers to add tuit

--- src/tuiter/tuits/tuit-list/index.js
+++ src/tuiter/tuits/tuit-list/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./index.css";
 import TuitItem from "../tuit-item";
 import {useSelector} from "react-redux";
+import WhatsHappening from "./whats-happening";
 
 const TuitList = () => {
     const tuitsArray = useSelector((state) => state.tuitsReducer);
     return (
         <>
+            <WhatsHappening/>
             {
                 tuitsArray.map(eachPost =>
                     <TuitItem {...eachPost} /> )
