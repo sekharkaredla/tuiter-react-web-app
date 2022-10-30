6823581c7584309e0b164c7a5273787d352cac25
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-10-30 02:06:23 -0400

    a7 challenge state editing done

--- src/tuiter/edit-profile/index.js
+++ src/tuiter/edit-profile/index.js
@@ -1,6 +1,41 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {updateProfile} from "../profile/profile-reducer";
+import {Link} from "react-router-dom";
+
 const EditProfile = () => {
+    const profileData = useSelector((state) => state.profileReducer);
+    const [profileState, setProfileState] = useState(profileData);
+    const dispatch = useDispatch();
+
+    const handleFieldChanges = (event) => {
+        const fieldName = event.target.name;
+        const changedValue = event.target.value;
+        const newProfile = {
+            ...profileData,
+            [fieldName]: changedValue
+        }
+        setProfileState(newProfile);
+    }
+
+    const handleSubmitButton = () => {
+        console.log(profileState);
+        dispatch(updateProfile(profileState));
+    }
+
     return (
-        <h1>Edit profile</h1>
+        <>
+            <label>Name : <input name="firstName" value={profileState.firstName} onChange={handleFieldChanges}/></label><br/>
+            <label>Bio : <textarea name="bio" value={profileState.bio} onChange={handleFieldChanges}/></label><br/>
+            <label>Location : <input name="location" value={profileState.location}
+                                     onChange={handleFieldChanges}/></label><br/>
+            <label>Website : <input name="website" value={profileState.website}
+                                    onChange={handleFieldChanges}/></label><br/>
+            <label>DOB : <input name="dateOfBirth" type="date" value={profileState.dateOfBirth}
+                                onChange={handleFieldChanges}/></label><br/>
+            <button onClick={handleSubmitButton}>Save</button><br/>
+            <Link to="/tuiter/profile">Back</Link>
+        </>
     );
 }
 
--- src/tuiter/profile/profile-reducer.js
+++ src/tuiter/profile/profile-reducer.js
@@ -4,6 +4,14 @@ import profile from "./profile.json"
 const profileSlice = createSlice({
     name: "profile",
     initialState: profile,
+    reducers: {
+        updateProfile(state, action) {
+            console.log(action.payload);
+            state = {...action.payload};
+            console.log(state);
+        }
+    }
 });
 
+export const {updateProfile} = profileSlice.actions;
 export default profileSlice.reducer;
