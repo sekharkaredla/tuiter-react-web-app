a9aa6ac5a7bc910b5b01e76f9dbc04923537cecc
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-04 16:49:23 -0400

    a7 challenge liked heart

--- src/tuiter/tuits/tuit-stats/index.js
+++ src/tuiter/tuits/tuit-stats/index.js
@@ -4,22 +4,22 @@ const TuitStats = ({post}) =>
     <>
         <div className="row">
             <div className="col-3 pt-2">
-                <a href="src/tuiter/tuits/tuit-stats/index#" className="text-decoration-none text-secondary">
+                <a href="#" className="text-decoration-none text-secondary">
                     <i className="fa-solid fa-comment"></i> {post.replies}
                 </a>
             </div>
             <div className="col-3 pt-2">
-                <a href="src/tuiter/tuits/tuit-stats/index#" className="text-decoration-none text-secondary">
+                <a href="#" className="text-decoration-none text-secondary">
                     <i className="fa fa-retweet" aria-hidden="true"></i> {post.retuits}
                 </a>
             </div>
             <div className="col-3 pt-2">
-                <a href="src/tuiter/tuits/tuit-stats/index#" className="text-decoration-none text-secondary">
-                    <i className="fa-solid fa-heart"></i> {post.likes}
+                <a href="#" className="text-decoration-none text-secondary">
+                    <i className={`fa-solid fa-heart ${post.liked ? "text-danger" : ""}`}></i> {post.likes}
                 </a>
             </div>
             <div className="col-3 pt-2">
-                <a href="src/tuiter/tuits/tuit-stats/index#" className="text-decoration-none text-secondary">
+                <a href="#" className="text-decoration-none text-secondary">
                     <i className="fa fa-upload" aria-hidden="true"></i>
                 </a>
             </div>
